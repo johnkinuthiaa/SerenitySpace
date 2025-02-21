078409724924e04aa1ccdd88fbfd635472737c07
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-02-21 12:30:43 +0300

    feat: added services for the models

---
+++ src/main/java/com/slippery/serenityspace/services/JournalService.java
@@ -0,0 +1,4 @@
+package com.slippery.serenityspace.services;
+
+public interface JournalService {
+}
---
+++ src/main/java/com/slippery/serenityspace/services/impl/CommunityServiceImplementation.java
@@ -0,0 +1,4 @@
+package com.slippery.serenityspace.services.impl;
+
+public class CommunityServiceImplementation {
+}
