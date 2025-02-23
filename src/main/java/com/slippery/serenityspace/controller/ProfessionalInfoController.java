da30cab4f35a5184292a3e5ad8b478fff5dfef99
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-02-23 13:47:22 +0300

    professional info controler

--- src/main/java/com/slippery/serenityspace/controller/ProfessionalInfoController.java
+++ src/main/java/com/slippery/serenityspace/controller/ProfessionalInfoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/professionalInfo")
-public class ProfessionalInfoController {
+public class ProfessionalInfoController{
     private final ProfessionalsService service;
 
     public ProfessionalInfoController(ProfessionalsService service) {
@@ -17,5 +17,26 @@ public class ProfessionalInfoController {
     @PostMapping("/create")
     public ResponseEntity<ProfessionalInfoDto> createNewProfessionalInfo(@RequestBody Professionals professionalsInfo){
         return ResponseEntity.ok(service.createNewProfessionalInfo(professionalsInfo));
+    }
+    @PutMapping("/update-info")
+    public ResponseEntity<ProfessionalInfoDto> updateProfessionalInfo(@RequestBody Professionals professionalsInfo,@RequestParam Long infoId) {
+        return ResponseEntity.ok(service.updateProfessionalInfo(professionalsInfo, infoId));
+
+    }
+    @GetMapping("/{infoId}/find")
+    public ResponseEntity<ProfessionalInfoDto> findProfessionalInfoById(@PathVariable Long infoId) {
+        return ResponseEntity.ok(service.findProfessionalInfoById(infoId));
+    }
+    @DeleteMapping("/{infoId}/delete")
+    public ResponseEntity<ProfessionalInfoDto> deleteProfessionalInfoById(@PathVariable Long infoId) {
+        return ResponseEntity.ok(service.deleteProfessionalInfoById(infoId));
+    }
+    @GetMapping("/get-all")
+    public ResponseEntity<ProfessionalInfoDto> findAllProfessionalInfos() {
+        return ResponseEntity.ok(service.findAllProfessionalInfos());
+    }
+    @DeleteMapping("/delete/all")
+    public ResponseEntity<ProfessionalInfoDto> deleteAllProfessionalInfos() {
+        return ResponseEntity.ok(service.deleteAllProfessionalInfos());
     }
 }
